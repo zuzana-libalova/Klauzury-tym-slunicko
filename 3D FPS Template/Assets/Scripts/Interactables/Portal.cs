1e353610f7f8754e4de8f8d56226de2b48cb5084
zuzana-libalova <4libalova.zuzana@skolamichael.cz>
2026-05-13 09:40:01 +0200

    portal fix, pridani interakci

--- 3D FPS Template/Assets/Scripts/Interactables/Portal.cs
+++ 3D FPS Template/Assets/Scripts/Interactables/Portal.cs
@@ -6,17 +6,31 @@ public class Portal : Interactable
     [Header("Portal Settings")]
     [Tooltip("The location where the player will be teleported to when interacting with the portal. / Pozice, kam bude hráč teleportován po interakci s portálem.")]
     public Vector3 teleportLocation = Vector3.zero;
+    bool teleported = false;
+    Collider player;
 
     public override void Interact(Collider other)
     {
         base.Interact(other);
         // Teleport the player to the specified location
-        other.transform.position = teleportLocation;
+        Debug.Log("player interact" + teleportLocation);
+        teleported = true;
+        player = other;
     }
 
     private void OnDrawGizmos() // Shows a debug sphere where to teleport player
     {
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(teleportLocation, 1f);
+    }
+
+    void FixedUpdate()
+    {
+        if (teleported) {
+            player.transform.position = teleportLocation;
+            teleported = false;
+        }
+
+        
     }
 }
