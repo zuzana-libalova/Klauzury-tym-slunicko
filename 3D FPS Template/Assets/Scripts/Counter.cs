d2e06dc366ad2aaa856ca261cc91406162db5d82
zuzana-libalova <4libalova.zuzana@skolamichael.cz>
2026-05-28 19:00:47 +0200

    ending png

--- 3D FPS Template/Assets/Scripts/Counter.cs
+++ 3D FPS Template/Assets/Scripts/Counter.cs
@@ -5,16 +5,14 @@ public class Counter : MonoBehaviour
 {
     public int talkedWith = 0;
 
-    private HashSet<GameObject> countedObjects = new HashSet<GameObject>();
+    
+    
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("TEXT"))
-            return;
-        if (!countedObjects.Contains(other.gameObject))
+        Debug.Log ("Testing");
+        if (other.CompareTag("TEXT"))
         {
-            countedObjects.Add(other.gameObject);
-
             talkedWith++;
             Debug.Log ("Interakce číslo" + talkedWith);
 
